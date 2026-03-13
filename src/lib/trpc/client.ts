4557f136b2770930f259c6050bcf35dd28a72fbd
Arop-Saas <aropsaas@gmail.com>
2026-03-12 20:12:20 -0600

    fix: export api alias from tRPC client

--- src/lib/trpc/client.ts
+++ src/lib/trpc/client.ts
@@ -4,3 +4,6 @@ import { createTRPCReact } from "@trpc/react-query";
 import { type AppRouter } from "@/server/routers/_app";
 
 export const trpc = createTRPCReact<AppRouter>();
+
+// alias used by some components
+export const api = trpc;
